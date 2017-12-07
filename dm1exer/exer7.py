f01664efc93643d480bc3501953c2f0b133e36af
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2017-12-07 12:14:05 +0900

    ex7

--- dm1exer/exer7.py
+++ dm1exer/exer7.py
@@ -17,25 +17,29 @@ img = np.float64(img)
 #出力画像を準備
 H = img.shape[0]
 W = img.shape[1]
-img_out = np.zeros((W,H), np.uint8)
+img_out = np.zeros((H,W), np.uint8)
 
 
 #誤差拡散法の計算
-for y in range(H)  :
-	for x in range( W)  : 
+for y in range(H-1)  :
+    for x in range(W-1)  :
         #二値化
-		if( img[y,x] > 127	) : 
-			img_out[y,x] = 255
-			e = img[y,x] - 255
-		else : 
-			img_out[y,x] = 0
-			e = img[y,x] - 0
+        if( img[y,x] > 127 ) :
+            img_out[y,x] = 255
+            e = img[y,x] - 255
+        else:
+            img_out[y,x] = 0
+            e = img[y,x] - 0
 
-		#誤差伝搬（誤差は元画像 img に足していく）
+        #誤差伝搬（誤差は元画像 img に足していく）
 
 
         #ここを編集
         #この課題，ヒント出しすぎかも
-	
+        img[y,x+1]+=e*5/16
+        img[y+1,x-1]+=e*3/16
+        img[y+1,x]+=e*5/16
+        img[y+1,x+1]+=e*3/16
+
 
 cv2.imwrite( fname_out, img_out);
