8564d112399d4040674a485baa0675bed0f9a2e7
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2017-12-07 12:10:49 +0900

    slove ex5

--- dm1exer/exer5.py
+++ dm1exer/exer5.py
@@ -12,6 +12,7 @@ fname_in  = sys.argv[1]
 R         = int(sys.argv[2])
 fname_out = sys.argv[3]
 
+
 #画像をロードしfloat型へ
 img = cv2.imread(fname_in)
 img = np.float64(img)
@@ -21,18 +22,19 @@ img = np.float64(img)
 
 #以下を編集（for文の範囲などは適宜変更すること）
 for y in range( int( img.shape[0] / R + 1) ) : 
-    for x in range( int( img.shape[1] / R + 1) ) : 
+    for x in range( int( img.shape[1] / R + 1) ) :
 
         #ヒント：スライス表現により画像の矩形領域を取り出せる 
-
         # 以下は，[y*R, (y+1)*R) x [y*R, (y+1)*R) の矩形領域のrチャンネル 
         rectR = img[y*R:(y+1)*R, x*R:(x+1)*R, 2]
-
-        print(rectR)
-
-        #代入もできる
-        if( x == 1 and y == 3) : 
-            img[y*R:(y+1)*R, x*R:(x+1)*R, 2] = 100
-
+        rectG = img[y*R:(y+1)*R, x*R:(x+1)*R, 1]
+        rectB = img[y*R:(y+1)*R, x*R:(x+1)*R, 0]
+        
+        
+        #代入もできる 
+        img[y*R:(y+1)*R, x*R:(x+1)*R, 2] = np.mean(rectR)
+        img[y*R:(y+1)*R, x*R:(x+1)*R, 1] = np.mean(rectG)
+        img[y*R:(y+1)*R, x*R:(x+1)*R, 0] = np.mean(rectB)
+            
         
 cv2.imwrite(fname_out, np.uint8( img) )
