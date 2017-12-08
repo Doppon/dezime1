89d3b0cdf939df232ae526ccb8951dcce035bee6
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2017-12-08 19:04:58 +0900

    slove ex2

--- dm1exer/exer2.py
+++ dm1exer/exer2.py
@@ -21,8 +21,12 @@ img_out = np.zeros_like( img )
 #フィルタ処理
 
 #ここを編集
+for y in range( 1, img.shape[0]-1 ) : #端っこは無視
+	for x in range( 1, img.shape[1]-1 ) : 
+		img_out[y,x] = 1/16*img[y-1,x-1]+2/16*img[y-1,x]+1/16*img[y-1,x+1]+\
+                       2/16*img[y  ,x-1]+4/16*img[y  ,x]+2/16*img[y  ,x+1]+\
+                       1/16*img[y+1,x-1]+2/16*img[y+1,x]+1/16*img[y+1,x+1]
 
 
-        
 #float型からuint8型に変換し、書き出し
 cv2.imwrite(fname_out, np.uint8( img_out) )
